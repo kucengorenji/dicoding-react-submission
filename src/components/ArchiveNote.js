33742cc4c0490c1f56b06666b13e16623e2ff5ab
mikael <mikelrisyad@gmail.com>
2022-09-03 21:36:03 +0700

    fix archive note

--- src/components/ArchiveNote.js
+++ src/components/ArchiveNote.js
@@ -7,33 +7,37 @@ const ArchiveNote = ({ data, onDeleteNote, onArchiveNote }) => {
     <>
       <h1 className="archive-title">ArchiveNote</h1>
       <div className="archive-container">
-        {activeNotes.map(({ id, title, createdAt, body }) => {
-          return (
-            <div className="archive-item" key={id}>
-              <div className="archive-item__title">{title}</div>
-              <div className="archive-item__date">{createdAt}</div>
-              <div className="archive-item__body">{body}</div>
-              <div className="archive-item__action">
-                <button
-                  onClick={() => {
-                    onDeleteNote(id);
-                  }}
-                  className="archive-item__action-delete"
-                >
-                  hapus
-                </button>
-                <button
-                  onClick={() => {
-                    onArchiveNote(id);
-                  }}
-                  className="archive-item__action-archive"
-                >
-                  arsipkan
-                </button>
+        {activeNotes.length !== 0 ? (
+          activeNotes.map(({ id, title, createdAt, body }) => {
+            return (
+              <div className="archive-item" key={id}>
+                <div className="archive-item__title">{title}</div>
+                <div className="archive-item__date">{createdAt}</div>
+                <div className="archive-item__body">{body}</div>
+                <div className="archive-item__action">
+                  <button
+                    onClick={() => {
+                      onDeleteNote(id);
+                    }}
+                    className="archive-item__action-delete"
+                  >
+                    hapus
+                  </button>
+                  <button
+                    onClick={() => {
+                      onArchiveNote(id);
+                    }}
+                    className="archive-item__action-archive"
+                  >
+                    arsipkan
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <h1>tidak ada catatan yang di arsipkan</h1>
+        )}
       </div>
     </>
   );
--- src/components/DisplayNote.js
+++ src/components/DisplayNote.js
@@ -6,33 +6,37 @@ const DisplayNote = ({ data, onDeleteNote, onArchiveNote }) => {
   return (
     <>
       <div className="note-container">
-        {activeNotes.map(({ id, title, createdAt, body }) => {
-          return (
-            <div className="note-item" key={id}>
-              <div className="note-item__title">{title}</div>
-              <div className="note-item__date">{createdAt}</div>
-              <div className="note-item__body">{body}</div>
-              <div className="note-item__action">
-                <button
-                  onClick={() => {
-                    onDeleteNote(id);
-                  }}
-                  className="note-item__action-delete"
-                >
-                  hapus
-                </button>
-                <button
-                  onClick={() => {
-                    onArchiveNote(id);
-                  }}
-                  className="note-item__action-archive"
-                >
-                  arsipkan
-                </button>
+        {activeNotes.length !== 0 ? (
+          activeNotes.map(({ id, title, createdAt, body }) => {
+            return (
+              <div className="note-item" key={id}>
+                <div className="note-item__title">{title}</div>
+                <div className="note-item__date">{createdAt}</div>
+                <div className="note-item__body">{body}</div>
+                <div className="note-item__action">
+                  <button
+                    onClick={() => {
+                      onDeleteNote(id);
+                    }}
+                    className="note-item__action-delete"
+                  >
+                    hapus
+                  </button>
+                  <button
+                    onClick={() => {
+                      onArchiveNote(id);
+                    }}
+                    className="note-item__action-archive"
+                  >
+                    arsipkan
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <h1>tidak ada catatan</h1>
+        )}
       </div>
     </>
   );
